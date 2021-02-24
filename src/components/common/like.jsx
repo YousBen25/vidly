c9a79d6a58dc23616f21740287925f96f9aa2b57
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-02-24 11:33:54 +0100

    Like button

---
+++ src/components/common/like.jsx
@@ -0,0 +1,17 @@
+// input: liked: boolean
+// output: onClick event
+
+const Like = (props) => {
+    let classes = "fa fa-heart";
+    if (!props.liked) classes += "-o";
+    return (
+      <i
+        onClick={props.onClick}
+        style={{ cursor: "pointer" }}
+        className={classes}
+        aria-hidden="true"
+      ></i>
+    );
+}
+
+export default Like;
