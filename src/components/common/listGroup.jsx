43fe486668e762cd744135f9d9ca5ab4617791b9
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-02-25 11:21:37 +0100

    Filtering - Handling Selection

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 const ListGroup = (props) => {
-  const { items, textProperty, valueProperty } = props;
+  const {
+    items,
+    textProperty,
+    valueProperty,
+    onItemSelect,
+    selectedItem,
+  } = props;
   return (
-    <ul class="list-group">
+    <ul className="list-group">
       {items.map((item) => (
-        <li class="list-group-item" key={item[valueProperty]}>
+        <li
+          onClick={() => onItemSelect(item)}
+          className={
+            item === selectedItem
+              ? "list-group-item active"
+              : "list-group-item"
+          }
+          key={item[valueProperty]}
+        >
           {item[textProperty]}
         </li>
       ))}
@@ -14,8 +28,8 @@ const ListGroup = (props) => {
 };
 
 ListGroup.defaultProps = {
-    textProperty: 'name',
-    valueProperty: '_id',
+  textProperty: "name",
+  valueProperty: "_id",
 };
 
 export default ListGroup;
